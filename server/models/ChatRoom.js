bb47b965619d50dc4a34e2923430798564644b50
AudreyLau8 <audrey.lau@swiftgoose.com>
2026-03-29 13:49:53 -0700

    saving progress here temporarily

--- server/models/ChatRoom.js
+++ server/models/ChatRoom.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-const chatRoomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
+const ChatRoomSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  subject: { type: String, required: true },
+  school: { type: String, required: true },
+  goal: { type: String, enum: ['exam prep', 'homework', 'general'], required: true },
+  intent: { type: String, enum: ['focus', 'help'], required: true },
+  participants: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
 
-const ChatRoom = mongoose.model('ChatRoom', chatRoomSchema);
-
-module.exports = ChatRoom;
+module.exports = mongoose.model('ChatRoom', ChatRoomSchema);
